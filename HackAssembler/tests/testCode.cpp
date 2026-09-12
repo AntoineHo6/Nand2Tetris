6ce60e96384cb436763402dda2ba79a8745838df
Antoine <antoineho6@gmail.com>
2026-09-12 18:37:15 -0400

    implemented Code::jump() with unit Tests

--- HackAssembler/tests/testCode.cpp
+++ HackAssembler/tests/testCode.cpp
@@ -11,27 +11,45 @@ void createTestFile(const std::string& filename, const std::string& content) {
 
 TEST_CASE("Testing Code::dest()") {
     SUBCASE("Extracts dest mnemonic into it's binary equivalent") {
-        std::string testPath = "test_standard.asm";
-        createTestFile(testPath, "DM=A+1\n");
+        std::string testPath = "codeDestTests.asm";
+        createTestFile(testPath, "DM=A+1\nD-1");
 
         Parser parser(testPath);
 
         parser.advance();
-
         CHECK(Code::dest(parser.dest()) == "011");
+
+        parser.advance();
+        CHECK(Code::dest(parser.dest()) == "000");
     };
 };
 
 
 TEST_CASE("Testing Code::comp()") {
+    // comp is mandatory, so we don't test if it's empty.
     SUBCASE("Extracts comp mnemonic into it's binary equivalent") {
-        std::string testPath = "test_standard.asm";
+        std::string testPath = "codeCompTests.asm";
         createTestFile(testPath, "DM=A+1\n");
 
         Parser parser(testPath);
 
         parser.advance();
-
         CHECK(Code::comp(parser.comp()) == "0011011");
+    };
+};
+
+
+TEST_CASE("Testing Code::jump()") {
+    SUBCASE("Extracts jump mnemonic into it's binary equivalent") {
+        std::string testPath = "codeJumpTests.asm";
+        createTestFile(testPath, "DM=A+1;JGT\nD-1");
+
+        Parser parser(testPath);
+
+        parser.advance();
+        CHECK(Code::jump(parser.jump()) == "001");
+
+        parser.advance();
+        CHECK(Code::jump(parser.jump()) == "000");
     };
 };
