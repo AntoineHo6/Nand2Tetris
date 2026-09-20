53aa52f82326efd1b068615b34cdf44e903cf337
Antoine <antoineho6@gmail.com>
2026-09-19 20:30:42 -0400

    can parse A_instructions and C_instructions with no symbols

--- HackAssembler/src/main.cpp
+++ HackAssembler/src/main.cpp
@@ -2,8 +2,10 @@
 #include <fstream>
 #include <string>
 #include <filesystem>
+#include <bitset>
 
 #include "parser.cpp"
+#include "code.cpp"
 
 int main(int argc, char* argv[]) {
     std::ifstream file(argv[1]);
@@ -22,7 +24,7 @@ int main(int argc, char* argv[]) {
 
     if (!hackFile.is_open()) {
         std::cerr << "Error: Could not create hack file!\n";
-        return;
+        return 1;
     }
 
     Parser parser(filePath);
@@ -30,10 +32,34 @@ int main(int argc, char* argv[]) {
     while (parser.hasMoreLines()) {
         parser.advance();
 
+        std::string binaryLine;
+        switch(parser.instructionType()) {
+            case InstrType::A_INSTRUCTION: {
+                int value = std::stoi(parser.symbol());
+                binaryLine = "0" + std::bitset<15>(value).to_string();
+                break;
+            }
+            case InstrType::C_INSTRUCTION: {
+                binaryLine = "111" + Code::comp(parser.comp())
+                                   + Code::dest(parser.dest()) 
+                                   + Code::jump(parser.jump());
+                break;                
+            }
+            case InstrType::L_INSTRUCTION: {
+                // TODO
+                continue;
+                break;
+            }
+        }
+
         // write to hackFile
-        // hackFile << binaryLine << '\n';
+        hackFile << binaryLine << '\n';
     }
 
     return 0;
+}
+
+std::string stringTo15BitBinary(const std::string& myString) {
+
 }
 
